f15a6d45e85bc28589101ba800574d9e051d7fdd
Sarthak Mishra <msarthak785@gmail.com>
2024-07-04 16:36:02 -0400

    UI improvements

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -3,7 +3,9 @@ import { SignInButton } from "@clerk/nextjs"
 export const Guest = () => {
     return(
         <div className="guest">
-            <h1>Know where your money goes. LimitPay: Expense tracking, simplified.</h1>
+            <h1>LimitPay</h1>
+            <h2>Expense tracking, simplified</h2>
+            <h3>Know where your money goes</h3>
             <p>Sign In to Limit Pay and manage your transactions</p>
             <SignInButton />
         </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { checkUser } from "@/lib/checkUser";
+import Link from "next/link";
 
 export const Header = async() => {
     const user = await checkUser();
@@ -8,6 +9,23 @@ export const Header = async() => {
         <nav className="navbar">
             <div className="navbar-container">
                 <h2>Limit Pay</h2>
+                <div className="navbar-items">
+                    <a
+                        href="https://github.com/sarthakmishraa/limitpay-expense-tracker"
+                        target="_blank">
+                            <span>Code</span>
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/sarthakmishraa/"
+                        target="_blank">
+                            <span>LinkedIn</span>
+                    </a>
+                    <a
+                        href="mailto:msarthak785@gmail.com"
+                        target="_blank">
+                            <span>Contact</span>
+                    </a>
+                </div>
                 <div>
                     <SignedOut>
                         <SignInButton />
